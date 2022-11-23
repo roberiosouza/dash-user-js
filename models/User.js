58d2e2c4fe5bc195aa632e1316b28639580aad05
roberio.dev <rroberiosouza@gmail.com>
2022-11-22 22:48:39 -0300

    Implementado Get e Set de usuário
    Implementado os métodos de getter e setter para a classe User.
    Atribuído a convenção de atributos privados.

--- models/User.js
+++ models/User.js
@@ -2,15 +2,89 @@ class User {
 
 	constructor(name, gender, birth, country, email, password, photo, admin){
 
-		this.name = name;
-		this.gender = gender;
-		this.birth = birth;
-		this.country = country;
-		this.email = email;
-		this.password = password;
-		this.photo = photo;
-		this.admin = admin;
+		this._name = name;
+		this._gender = gender;
+		this._birth = birth;
+		this._country = country;
+		this._email = email;
+		this._password = password;
+		this._photo = photo;
+		this._admin = admin;
+		this._register = new Date();
 
+	}
+
+	get register(){
+		return this._register;
+	}
+
+	get name(){
+		return this._name;
+	}
+
+	get gender(){
+		return this._gender;
+	}
+
+	get birth(){
+		return this._birth;
+	}
+
+	get country(){
+		return this._country;
+	}
+
+	get email(){
+		return this._email;
+	}
+
+	get password(){
+		return this._password;
+	}	
+
+	get photo(){
+		return this._photo;
+	}	
+
+	get admin(){
+		return this._admin;
+	}
+
+
+	set register(register){
+		this._register = register;
+	}
+
+	set name(name){
+		this._name = name;
+	}
+
+	set gender(){
+		return this._gender;
+	}
+
+	set birth(birth){
+		this._birth = birth;
+	}
+
+	set country(country){
+		this._country = country;
+	}
+
+	set email(email){
+		this._email = email;
+	}
+
+	set password(password){
+		this._password = password;
+	}	
+
+	set photo(photo){
+		this._photo = photo;
+	}
+
+	set admin(admin){
+		this._admin = admin;
 	}
 
 }
